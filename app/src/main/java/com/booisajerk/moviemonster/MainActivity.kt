28a99a7b65df05d92346536955d9940d74d1497b
Jenn Parker <jenniferparker070@gmail.com>
2018-10-23 19:18:55 -0700

    Add About screen and functionality

--- app/src/main/java/com/booisajerk/moviemonster/MainActivity.kt
+++ app/src/main/java/com/booisajerk/moviemonster/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.booisajerk.moviemonster
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,8 +22,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_info -> {
-                Toast.makeText(this, "Info click placeholder", Toast.LENGTH_LONG).show()
+            R.id.action_about -> {
+                val aboutIntent = Intent(this, AboutActivity::class.java)
+                startActivity(aboutIntent)
                 true
             }
             else -> super.onOptionsItemSelected(item)
