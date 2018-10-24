4405eacac17e1fb22cef3100c79c7de34f116cd5
Jenn Parker <jenniferparker070@gmail.com>
2018-10-24 10:43:51 -0700

    Add Splash screen

---
+++ app/src/main/java/com/booisajerk/moviemonster/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.booisajerk.moviemonster
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Show splash screen for 2 seconds.
+        // To show the splash only as the app is loading, retain only the intent code.
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
